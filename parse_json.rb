6d7b6e74dad23f6dd26d2e532c9eee10082beaf2
Rene Weteling <rene.weteling@t-mobile.nl>
2023-03-08 16:48:01 +0100

    test

--- parse_json.rb
+++ parse_json.rb
@@ -5,13 +5,13 @@ require 'date'
 Dir.glob('/mnt/c/Users/ID055730/Downloads/Takeout/Google Foto_s/**/*.json').each do |json|
   JSON.parse(File.open(json).read, {:symbolize_names => true}) => {title:, photoTakenTime: {timestamp:}}
   
-  data = "#{DateTime.strptime(timestamp,'%s').strftime("%FT%TZ")}, #{title.sub(/^NS_/, "")}\n"
+  data = "#{DateTime.strptime(timestamp,'%s').strftime("%FT%TZ")}, #{title.sub(/^ns_/, "")}\n"
 
   File.write("pictures.csv", data, mode: "a")
-
+  puts "."
   rescue
-
-    binding.pry
+    pp JSON.parse(File.open(json).read, {:symbolize_names => true})
+    # binding.pry
 end
 
 
